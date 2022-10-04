a4db5a05bb8c5ef3ed7072d53b125fa7463022d9
Kyeongrok <oceanfog1@gmail.com>
2022-10-05 08:32:25 +0900

    예제파일 추가 1.주문목록
    2.파트너목록

---
+++ src/main/java/week4/Sqrt.java
@@ -0,0 +1,20 @@
+package week4;
+
+public class Sqrt {
+
+    public long solution(long n) {
+        long answer = 0;
+        // n = 121
+        for (int i = 1; i < n; i++) {
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sqrt sqrt = new Sqrt();
+        sqrt.solution(121);
+
+    }
+}
