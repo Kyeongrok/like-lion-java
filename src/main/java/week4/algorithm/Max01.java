b14790433798955333618950e80e3f309d4651ab
Kyeongrok <oceanfog1@gmail.com>
2022-10-12 09:07:35 +0900

    Max01

---
+++ src/main/java/week4/algorithm/Max01.java
@@ -0,0 +1,8 @@
+package week4.algorithm;
+
+public class Max01 {
+    public static void main(String[] args) {
+        // arr선언
+        int[] arr = new int[]{3, 29, 38, 12, 57, 74, 40, 85, 61};
+    }
+}
