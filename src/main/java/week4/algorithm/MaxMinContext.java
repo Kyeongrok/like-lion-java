f20b079c574c51c865043d00a021df2d7eb9a7c4
Kyeongrok <oceanfog1@gmail.com>
2022-10-12 14:45:39 +0900

    MaxAndMin

--- src/main/java/week4/algorithm/MaxMinContext.java
+++ src/main/java/week4/algorithm/MaxMinContext.java
@@ -1,6 +1,6 @@
 package week4.algorithm;
 
-interface isCompare {
+interface IsCompare {
     boolean isCompare(int a, int b);
 }
 public class MaxMinContext {
@@ -12,16 +12,25 @@ public class MaxMinContext {
         return a < b;
     }
 
-    public int[] getNum(int[] arr) {
+    public int[] getNum(int[] arr, IsCompare isCompare) {
         //loop 구성
         int maxIdx = 0;
         int maxValue = arr[0]; // arr이 모두 음수인 경우 0으로 하면 0이 max
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > maxValue) {
+            if (isCompare.isCompare(arr[i], maxValue)) {
                 maxValue = arr[i];
                 maxIdx = i;
             }
         }
         return new int[]{maxValue, maxIdx};
+    }
+
+    public int[] max(int[] arr) {
+        return getNum(arr, new IsCompare() {
+            @Override
+            public boolean isCompare(int a, int b) {
+                return a > b;
+            }
+        });
     }
 }
